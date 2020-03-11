3745a99d011fd1bb25ede6a4a0937bad9297c78d
James Simpson <simpsonj948@gmail.com>
2020-03-11 23:15:34 +0000

    Models created, and registered with the admin site

--- kitup/admin.py
+++ kitup/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from kitup.models import Users, Player, Match, Report, Sport
 
 # Register your models here.
+
+admin.site.register(Users)
+admin.site.register(Player)
+admin.site.register(Match)
+admin.site.register(Report)
+admin.site.register(Sport)
--- kitup/models.py
+++ kitup/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
+
+
+class Users(models.Model):
+    user_id = models.UUIDField(primary_key=True)
+    forename = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    email = models.EmailField(max_length=100, unique=True)
+    password = models.CharField(max_length=256)
+    picture = models.ImageField()
+    rating = models.IntegerField()
+    strikes = models.IntegerField()
+    joined_on = models.DateTimeField(default=timezone.now())
+    last_login = models.DateTimeField()
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.forename + " " + self.surname
+
+
+class Sport(models.Model):
+    sport_id = models.UUIDField(primary_key=True)
+    name = models.CharField(max_length=30)
+    team_size = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Match(models.Model):
+    match_id = models.UUIDField(primary_key=True)
+    name = models.CharField(max_length=30)
+    sport_id = models.ForeignKey(Sport, on_delete=models.CASCADE)
+    min_age = models.IntegerField(default=18)
+    max_age = models.IntegerField()
+    min_rating = models.IntegerField()
+    team_home_score = models.IntegerField()
+    team_away_score = models.IntegerField()
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
+
+
+class Player(models.Model):
+    player_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    game_id = models.ForeignKey(Match, on_delete=models.CASCADE)
+    team = models.IntegerField()
+
+    class Meta:
+        unique_together = ('player_id', 'game_id')
+
+
+class Report(models.Model):
+    report_id = models.UUIDField(primary_key=True)
+    report_maker = models.IntegerField()
+    report_subject = models.IntegerField()
+    reason = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.report_id
