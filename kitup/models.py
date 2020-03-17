da9dbf17bbce7f9be9d11b583e721e6281370a91
dans-acc <danv123456789@gmail.com>
2020-03-17 12:43:06 +0000

    Fixed models

--- kitup/models.py
+++ kitup/models.py
@@ -1,35 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
-# Profile defines the individual users registered for the site.
-# Model extends the default User model implemented by django.
+# Profile defines the individual user's registered for the site.
+# Fundamentally, the model extends the default User model implemented by django.
 class Profile(models.Model):
 
-    # Create a one-to-one-link between the profile and the user.
+    # Additional user attributes.
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    # Additional user information.
     profile_picture = models.ImageField(upload_to='profile_images')
     age = models.IntegerField()
-
-    # Game specific information.
     rating = models.IntegerField(default=0)
     strikes = models.IntegerField(default=0)
 
+    # Model meta data.
     class Meta:
         verbose_name_plural = 'Profiles'
 
-    # Make use of the user models username.
+    # Provide a user friendly name for the model.
     def __str__(self):
         return self.user.username
 
-# Defines the sport table i.e. the game type.
+# Model defines the type of sport that can be played.
 class Sport(models.Model):
 
+    # Define sport model constants.
     NAME_MAX_LENGTH = 30
 
-    # The name of the sport and the maximum team sizes.
+    # The name of the sport and individual team-sizes.
     name = models.CharField(max_length=NAME_MAX_LENGTH)
     max_team_size = models.IntegerField()
 
@@ -37,41 +34,41 @@ class Sport(models.Model):
     def __str__(self):
         return self.name
 
-# Defines the match table; contains information pertaining to created matches.
+# Model defines a match that has / is going to happened.
 class Match(models.Model):
 
+    # Defines the Match models constants.
     NAME_MAX_LENGTH = 30
-
     DEFAULT_MIN_AGE = 16
     DEFAULT_MAX_AGE = 68
     DEFAULT_MIN_RATING = 0
 
     # Attributes pertaining to the match type.
-    sport = models.ForeignKey(Sport, on_delete=models.CASCADE)
-    name = models.CharField(max_length=NAME_MAX_LENGTH)
-    match_owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    sport = models.ForeignKey(Sport, on_delete=models.CASCADE, null=False)
+    name = models.CharField(max_length=NAME_MAX_LENGTH, null=False)
+    match_owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=False)
 
-    start_datetime = models.DateTimeField(null=True)
-    duration = models.IntegerField()
+    # THIS MIGHT GET REMOVED DUE TO US USING THE GOOGLE API!
+    match_photo = models.ImageField(upload_to='match_images')
 
-    # Age restructions for the match.
+    # Match times.
+    start_datetime = models.DateTimeField(null=False)
+    end_time = models.TimeField(auto_now=False)
+
+    # Restrictions on joining the match.
     min_age = models.IntegerField(default=DEFAULT_MIN_AGE)
     max_age = models.IntegerField(default=DEFAULT_MAX_AGE)
-
-    # Rating restrictions for the match.
     min_rating = models.IntegerField(default=DEFAULT_MIN_RATING)
 
-    # Post match team values.
+    # Post match team scores.
     team_home_score = models.IntegerField(default=0)
     team_away_score = models.IntegerField(default=0)
 
-    # Match customisation
-    match_photo = models.ImageField(upload_to='match_images')
-
+    # Model meta data.
     class Meta:
         verbose_name_plural = 'Matches'
 
-    # Returns the name of the match.
+    # Returns a user friendly name for the match.
     def __str__(self):
         return self.name
 
@@ -81,14 +78,11 @@ class Player(models.Model):
     # The user and the match for which this entry exists.
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     match = models.ForeignKey(Match, on_delete=models.CASCADE)
-
-    # The team to which they belong.
     team = models.IntegerField()
 
     # Defines meta attributes pertaining to the Player model.
     class Meta:
         verbose_name_plural = 'Players'
-        unique_together = ('user', 'match')
 
     # Defines the name of the model.
     def __str__(self):
@@ -97,15 +91,15 @@ class Player(models.Model):
 # Defines the model used to report other users.
 class Report(models.Model):
 
+    # Defines report models constants.
     REASON_MAX_LENGTH = 60
 
-    # Defines the reporting and reported users.
+    # The user making the report, the user in question, and the reason for the report, respectively.
     reporting_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reporting_user')
     reported_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reported_user')
-
-    # The reason for the report.
     reason = models.CharField(max_length = REASON_MAX_LENGTH)
 
+    # Report models meta data.
     class Meta:
         verbose_name_plural = 'Reports'
 
